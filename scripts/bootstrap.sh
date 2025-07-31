0d9e1676e2e384b8f13989cbcb28a92aa4e13b7f
Trevor Royer <troyer@redhat.com>
2025-07-31 17:31:39 -0600

    stub out initial helm deployment scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -79,12 +79,64 @@ deploy_example(){
     kustomize build ${chose_example_overlay_path} | oc apply -n ${ARGOCD_NS} -f -
 }
 
+deploy_example_with_helm(){
+    if [ -z "$1" ]; then
+        echo "Error: No option provided to deploy_example()"
+        echo "Usage: deploy_example <chose_example_overlay_path>"
+        exit 1
+    fi
+    chose_example_overlay_path=$1
 
-main(){
-    choose_example
-    choose_example_option ${CHOSEN_EXAMPLE_PATH}
-    deploy_example ${CHOSEN_EXAMPLE_OPTION_PATH}
+    example_name="blah"
+
+    helm install ${example_name} ./charts/argocd-appgenerator -n ${ARGOCD_NS} \
+        --set repoURL=${GITHUB_URL} \
+        --set revision=${GIT_BRANCH} \
+        --set directories[0].path=${chose_example_overlay_path}
 }
 
-check_oc_login
+
+set_repo_url(){
+    GIT_REPO=$(git config --get remote.origin.url)
+    GIT_REPO_BASENAME=$(get_git_basename ${GIT_REPO})
+    GITHUB_URL="https://github.com/${GIT_REPO_BASENAME}.git"
+    
+    echo
+    echo "Current repository URL: ${GITHUB_URL}"
+    echo
+    read -p "Press Enter to use this URL, or enter a new repository URL: " user_input
+    
+    if [ -n "$user_input" ]; then
+        GITHUB_URL="$user_input"
+        echo "Updated repository URL to: ${GITHUB_URL}"
+    else
+        echo "Using repository URL: ${GITHUB_URL}"
+    fi
+}
+
+set_repo_branch(){
+    GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+    echo
+    echo "Current repository branch: ${GIT_BRANCH}"
+    echo
+    read -p "Press Enter to use this branch, or enter a new repository branch: " user_input
+    
+    if [ -n "$user_input" ]; then
+        GIT_BRANCH="$user_input"
+        echo "Updated repository branch to: ${GIT_BRANCH}"
+    else
+        echo "Using repository branch: ${GIT_BRANCH}"
+    fi
+}
+
+main(){
+    set_repo_url
+    set_repo_branch
+    # choose_example
+    # choose_example_option ${CHOSEN_EXAMPLE_PATH}
+    # deploy_example ${CHOSEN_EXAMPLE_OPTION_PATH}
+}
+
+# check_oc_login
 main
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -7,3 +7,16 @@ check_oc_login(){
   oc whoami || exit 1
   echo
 }
+
+get_git_basename(){
+  if [ -z "$1" ]; then
+    echo "No repo provided."
+    exit 1
+  else
+    REPO_URL=$1
+  fi
+
+  QUERY='s#(git@|https://)github.com[:/]([a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+)\.git#\2#'
+  REPO_BASENAME=$(echo ${REPO_URL} | sed -E  ${QUERY})
+  echo ${REPO_BASENAME}
+}
